8ffb0541bea7b737badee00809576eeec9ddf627
Tim Kendrick <timkendrick@gmail.com>
2015-05-22 14:58:46 +0100

    [helpers/expandConfigPlaceholders] Allow non-string placeholder values

--- test/spec/helpers/expandConfigPlaceholders.spec.js
+++ test/spec/helpers/expandConfigPlaceholders.spec.js
@@ -6,37 +6,316 @@ var expect = chai.expect;
 var expandConfigPlaceholders = require('../../../lib/helpers/expandConfigPlaceholders');
 
 describe('helpers.expandConfigPlaceholders()', function() {
-	it('should handle no placeholders and return a copy', function() {
+	it('should return a copy of the original object', function() {
+		var input, expected, actual;
+
+		input = {
+			template: {},
+			context: {}
+		};
+		expected = {};
+		actual = expandConfigPlaceholders(input.template, input.context);
+		expect(actual).to.eql(expected);
+		expect(actual).not.to.equal(expected);
+	});
+
+	it('should copy primitive values', function() {
 		var input, expected, actual;
 
 		input = {
 			template: {
-				message: 'Hello, world!'
+				string: 'Hello, world!',
+				empty: '',
+				true: true,
+				false: false,
+				0: 0,
+				1: 1,
+				2: 2,
+				undefined: undefined,
+				null: null
 			},
 			context: {}
 		};
 		expected = {
-			message: 'Hello, world!'
+			string: 'Hello, world!',
+			empty: '',
+			true: true,
+			false: false,
+			0: 0,
+			1: 1,
+			2: 2,
+			undefined: undefined,
+			null: null
 		};
 		actual = expandConfigPlaceholders(input.template, input.context);
 		expect(actual).to.eql(expected);
-		expect(actual).not.to.equal(input.template);
+	});
+
+	it('should copy dates', function() {
+		var input, expected, actual;
+
+		var date = new Date();
+		input = {
+			template: {
+				date: date
+			},
+			context: {}
+		};
+		var result = expandConfigPlaceholders(input.template, input.context);
+
+		actual = result.date;
+		expected = Date;
+		expect(actual).to.be.an.instanceof(expected);
+
+		expected = result.date.getTime();
+		actual = date.getTime();
+		expect(actual).to.equal(expected);
+
+		actual = result.date;
+		expected = date;
+		expect(actual).not.to.equal(expected);
+	});
+
+	it('should copy regular expressions', function() {
+		var input, expected, actual;
+
+		var regexp = /Hello, world!/gim;
+		input = {
+			template: {
+				regexp: regexp
+			},
+			context: {}
+		};
+		var result = expandConfigPlaceholders(input.template, input.context);
+
+		actual = result.regexp;
+		expected = RegExp;
+		expect(actual).to.be.an.instanceof(expected);
+
+		expected = '/Hello, world!/gim';
+		actual = result.regexp.toString();
+		expect(actual).to.equal(expected);
+
+		actual = result.regexp;
+		expected = regexp;
+		expect(actual).not.to.equal(expected);
+	});
+
+	it('should copy arrays', function() {
+		var input, expected, actual;
 
 		input = {
 			template: {
-				message: 'Hello, world!'
+				messages: [
+					'Hello, world!',
+					'Goodbye, world!'
+				]
 			},
-			context: null
+			context: {}
+		};
+		expected = {
+			messages: [
+				'Hello, world!',
+				'Goodbye, world!'
+			]
+		};
+		var result = expandConfigPlaceholders(input.template, input.context);
+		actual = result;
+		expect(actual).to.eql(expected);
+
+		actual = result.messages;
+		expected = input.template.messages;
+		expect(actual).not.to.equal(expected);
+	});
+
+	it('should copy objects', function() {
+		var input, expected, actual;
+
+		input = {
+			template: {
+				messages: {
+					hello: 'Hello, world!',
+					goodbye: 'Goodbye, world!'
+				}
+			},
+			context: {}
+		};
+		expected = {
+			messages: {
+				hello: 'Hello, world!',
+				goodbye: 'Goodbye, world!'
+			}
+		};
+		actual = expandConfigPlaceholders(input.template, input.context);
+		expect(actual).to.eql(expected);
+	});
+
+	it('should copy non-primitive values by reference', function() {
+		var input, expected, actual;
+
+		var testFunction = function test() {};
+		var testInstance = new (function TestClass() { })();
+
+		input = {
+			template: {
+				message: 'Hello, <%= user.name %>!',
+				function: testFunction,
+				instance: testInstance
+			},
+			context: {
+				user: {
+					name: 'world'
+				}
+			}
+		};
+
+		actual = expandConfigPlaceholders(input.template, input.context);
+		expected = {
+			message: 'Hello, world!',
+			function: testFunction,
+			instance: testInstance
+		};
+		expect(actual).to.eql(expected);
+		expect(actual.function).to.equal(testFunction);
+		expect(actual.instance).to.equal(testInstance);
+	});
+
+	it('should handle nested objects and arrays', function() {
+		var input, expected, actual;
+
+		input = {
+			template: {
+				object: {
+					array: [
+						'Hello, world!',
+						'Goodbye, world!'
+					]
+				},
+				array: [
+					{
+						hello: 'Hello, world!',
+						goodbye: 'Goodbye, world!'
+					}
+				]
+			},
+			context: {}
+		};
+		expected = {
+			object: {
+				array: [
+					'Hello, world!',
+					'Goodbye, world!'
+				]
+			},
+			array: [
+				{
+					hello: 'Hello, world!',
+					goodbye: 'Goodbye, world!'
+				}
+			]
+		};
+		actual = expandConfigPlaceholders(input.template, input.context);
+		expect(actual).to.eql(expected);
+	});
+
+	it('should expand placeholder values', function() {
+		var input, expected, actual;
+
+		input = {
+			template: {
+				message: '<%= message %>'
+			},
+			context: {
+				message: 'Hello, world!'
+			}
 		};
 		expected = {
 			message: 'Hello, world!'
 		};
 		actual = expandConfigPlaceholders(input.template, input.context);
 		expect(actual).to.eql(expected);
-		expect(actual).not.to.equal(input.template);
 	});
 
-	it('should expand placeholders and return a copy', function() {
+	it('should expand nested values in placeholders', function() {
+		var input, expected, actual;
+
+		input = {
+			template: {
+				message: '<%= message.value %>'
+			},
+			context: {
+				message: {
+					value: 'Hello, world!'
+				}
+			}
+		};
+		expected = {
+			message: 'Hello, world!'
+		};
+		actual = expandConfigPlaceholders(input.template, input.context);
+		expect(actual).to.eql(expected);
+	});
+
+	it('should expand expressions in placeholders', function() {
+		var input, expected, actual;
+
+		input = {
+			template: {
+				message: '<%= message === "Hello, world!" ? "Goodbye, world!" : message %>'
+			},
+			context: {
+				message: 'Hello, world!'
+			}
+		};
+		expected = {
+			message: 'Goodbye, world!'
+		};
+		actual = expandConfigPlaceholders(input.template, input.context);
+		expect(actual).to.eql(expected);
+	});
+
+	it('should expand placeholders in nested objects', function() {
+		var input, expected, actual;
+
+		input = {
+			template: {
+				object: {
+					array: [
+						'<%= hello %>',
+						'<%= goodbye %>'
+					]
+				},
+				array: [
+					{
+						hello: '<%= hello %>',
+						goodbye: '<%= goodbye %>'
+					}
+				]
+			},
+			context: {
+				hello: 'Hello, world!',
+				goodbye: 'Goodbye, world!'
+			}
+		};
+		expected = {
+			object: {
+				array: [
+					'Hello, world!',
+					'Goodbye, world!'
+				]
+			},
+			array: [
+				{
+					hello: 'Hello, world!',
+					goodbye: 'Goodbye, world!'
+				}
+			]
+		};
+		actual = expandConfigPlaceholders(input.template, input.context);
+		expect(actual).to.eql(expected);
+	});
+
+	it('should expand placeholders in interpolated strings', function() {
 		var input, expected, actual;
 
 		input = {
@@ -52,10 +331,9 @@ describe('helpers.expandConfigPlaceholders()', function() {
 		};
 		actual = expandConfigPlaceholders(input.template, input.context);
 		expect(actual).to.eql(expected);
-		expect(actual).not.to.equal(input.template);
 	});
 
-	it('should expand nested placeholders', function() {
+	it('should expand nested values in interpolated strings', function() {
 		var input, expected, actual;
 
 		input = {
@@ -73,6 +351,126 @@ describe('helpers.expandConfigPlaceholders()', function() {
 		};
 		actual = expandConfigPlaceholders(input.template, input.context);
 		expect(actual).to.eql(expected);
+	});
+
+	it('should expand expressions in interpolated strings', function() {
+		var input, expected, actual;
+
+		input = {
+			template: {
+				message: 'Hello, <%= user.name === "world" ? "there" : user.name %>!'
+			},
+			context: {
+				user: {
+					name: 'world'
+				}
+			}
+		};
+		expected = {
+			message: 'Hello, there!'
+		};
+		actual = expandConfigPlaceholders(input.template, input.context);
+		expect(actual).to.eql(expected);
+	});
+
+	it('should expand placeholders in nested interpolated strings', function() {
+		var input, expected, actual;
+
+		input = {
+			template: {
+				object: {
+					array: [
+						'Hello, <%= user %>!',
+						'Goodbye, <%= user %>!'
+					]
+				},
+				array: [
+					{
+						hello: 'Hello, <%= user %>!',
+						goodbye: 'Goodbye, <%= user %>!'
+					}
+				]
+			},
+			context: {
+				user: 'world'
+			}
+		};
+		expected = {
+			object: {
+				array: [
+					'Hello, world!',
+					'Goodbye, world!'
+				]
+			},
+			array: [
+				{
+					hello: 'Hello, world!',
+					goodbye: 'Goodbye, world!'
+				}
+			]
+		};
+		actual = expandConfigPlaceholders(input.template, input.context);
+		expect(actual).to.eql(expected);
+	});
+
+	it('should expand non-string placeholder values', function() {
+		var input, expected, actual;
+
+		var testDate = new Date();
+		var testRegexp = /Hello, world!/gim;
+		var testObject = {};
+		var testArray = [];
+		var testFunction = function() { };
+		var testInstance = new (function Test() { })();
+		input = {
+			template: {
+				yes: '<%= yes %>',
+				no: '<%= no %>',
+				zero: '<%= zero %>',
+				one: '<%= one %>',
+				two: '<%= two %>',
+				date: '<%= date %>',
+				regexp: '<%= regexp %>',
+				object: '<%= object %>',
+				array: '<%= array %>',
+				function: '<%= fn %>',
+				instance: '<%= instance %>'
+			},
+			context: {
+				yes: true,
+				no: false,
+				zero: 0,
+				one: 1,
+				two: 2,
+				date: testDate,
+				regexp: testRegexp,
+				object: testObject,
+				array: testArray,
+				fn: testFunction,
+				instance: testInstance
+			}
+		};
+		expected = {
+			yes: true,
+			no: false,
+			zero: 0,
+			one: 1,
+			two: 2,
+			date: testDate,
+			regexp: testRegexp,
+			object: testObject,
+			array: testArray,
+			function: testFunction,
+			instance: testInstance
+		};
+		actual = expandConfigPlaceholders(input.template, input.context);
+		expect(actual).to.eql(expected);
+		expect(actual.date).to.equal(testDate);
+		expect(actual.regexp).to.equal(testRegexp);
+		expect(actual.object).to.equal(testObject);
+		expect(actual.array).to.equal(testArray);
+		expect(actual.function).to.equal(testFunction);
+		expect(actual.instance).to.equal(testInstance);
 	});
 
 	it('should throw errors for invalid placeholders', function() {
@@ -98,6 +496,24 @@ describe('helpers.expandConfigPlaceholders()', function() {
 			context: null
 		};
 		expected = Error;
+		actual = function() {
+			return expandConfigPlaceholders(input.template, input.context);
+		};
+		expect(actual).to.throw(expected);
+	});
+
+	it('should throw an error if placeholder contains syntax errors', function() {
+		var input, expected, actual;
+
+		input = {
+			template: {
+				invalid: '<%= syntax. %>'
+			},
+			context: {
+				syntax: {}
+			}
+		};
+		expected = SyntaxError;
 		actual = function() {
 			return expandConfigPlaceholders(input.template, input.context);
 		};
