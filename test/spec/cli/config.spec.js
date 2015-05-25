999738b2e18bd210792341bbe137787b0828f5dd
Tim Kendrick <timkendrick@gmail.com>
2015-05-25 23:42:13 +0100

    [cli/config] Add `config get` and `config set` commands

--- test/spec/cli/config.spec.js
+++ test/spec/cli/config.spec.js
@@ -5,6 +5,7 @@ var expect = chai.expect;
 var chaiAsPromised = require('chai-as-promised');
 var sinonChai = require('sinon-chai');
 var rewire = require('rewire');
+var mockCli = require('mock-cli');
 
 var InvalidArgumentsError = require('../../../lib/errors').InvalidArgumentsError;
 
@@ -22,129 +23,389 @@ describe('cli.config()', function() {
 		cliConfig.__set__('Api', MockApi);
 	});
 
-	it('should throw an error if no configuration updates are specified', function() {
-		var args = [];
-		var options = {
-			environment: 'goodbye'
-		};
-		var expected = InvalidArgumentsError;
-		var actual = cliConfig(args, options);
-		expect(actual).to.be.rejectedWith(expected);
+	var unmockCli = null;
+	afterEach(function() {
+		if (unmockCli) {
+			unmockCli();
+			unmockCli = null;
+		}
+		MockApi.reset();
 	});
 
-	it('should update default environment configuration when no package is specified', function() {
-		var args = [];
-		var options = {
-			config: {
-				hello: 'world'
-			},
-			environment: 'goodbye'
-		};
-		var expected = {
-			updates: {
-				hello: 'world'
-			},
-			environment: 'goodbye'
-		};
-		return cliConfig(args, options)
-			.then(function(returnValue) {
-				expect(MockApi.instance.updateEnvironmentConfig).to.have.been.calledWith(expected);
-			});
+	describe('basic operation', function() {
+
+		it('should throw an error if no operation is specified', function() {
+			var args = [];
+			var options = {};
+			var promise = cliConfig(args, options);
+			return expect(promise).to.be.rejectedWith(InvalidArgumentsError);
+		});
+
+		it('should throw an error if an invalid operation is specified', function() {
+			var args = ['nonexistent'];
+			var options = {};
+			var promise = cliConfig(args, options);
+			return expect(promise).to.be.rejectedWith(InvalidArgumentsError);
+		});
 	});
 
-	it('should update package configuration if an external package is specified', function() {
-		var args = [];
-		var options = {
-			package: 'my-package',
-			config: {
-				hello: 'world'
-			},
-			environment: 'goodbye'
-		};
-		var expected = {
-			package: 'my-package',
-			updates: {
-				hello: 'world'
-			},
-			environment: 'goodbye'
-		};
-		return cliConfig(args, options)
-			.then(function(returnValue) {
-				expect(MockApi.instance.updatePackageConfig).to.have.been.calledWith(expected);
-			});
+	describe('config get', function() {
+
+		it('should get the default environment configuration', function() {
+			unmockCli = mockCli();
+
+			var args = ['get'];
+			var options = {};
+			MockApi.stubs.environmentConfig = {
+				message: 'Hello, world!'
+			};
+			return cliConfig(args, options)
+				.then(function(returnValue) {
+					var cli = unmockCli();
+
+					expect(returnValue).to.eql({
+						message: 'Hello, world!'
+					});
+					expect(cli.stdout).to.equal(JSON.stringify({
+						message: 'Hello, world!'
+					}, null, 2) + '\n');
+					expect(MockApi.instance.getEnvironmentConfig).to.have.been.calledWith({
+						environment: null,
+						expand: false
+					});
+				});
+		});
+
+		it('should get custom environment configuration', function() {
+			unmockCli = mockCli();
+
+			var args = ['get'];
+			var options = {
+				env: 'custom'
+			};
+			MockApi.stubs.environmentConfig = {
+				message: 'Hello, world!'
+			};
+			return cliConfig(args, options)
+				.then(function(returnValue) {
+					var cli = unmockCli();
+
+					expect(returnValue).to.eql({
+						message: 'Hello, world!'
+					});
+					expect(cli.stdout).to.equal(JSON.stringify({
+						message: 'Hello, world!'
+					}, null, 2) + '\n');
+					expect(MockApi.instance.getEnvironmentConfig).to.have.been.calledWith({
+						environment: 'custom',
+						expand: false
+					});
+				});
+		});
+
+		it('should get package configuration', function() {
+			unmockCli = mockCli();
+
+			var args = ['get'];
+			var options = {
+				package: 'package'
+			};
+			MockApi.stubs.packageConfig = {
+				message: 'Hello, world!'
+			};
+			return cliConfig(args, options)
+				.then(function(returnValue) {
+					var cli = unmockCli();
+
+					expect(returnValue).to.eql({
+						message: 'Hello, world!'
+					});
+					expect(cli.stdout).to.equal(JSON.stringify({
+						message: 'Hello, world!'
+					}, null, 2) + '\n');
+					expect(MockApi.instance.getPackageConfig).to.have.been.calledWith({
+						package: 'package',
+						environment: null,
+						expand: false
+					});
+				});
+		});
+
+		it('should get local task configuration, default target', function() {
+			unmockCli = mockCli();
+
+			var args = ['get'];
+			var options = {
+				task: 'task'
+			};
+			MockApi.stubs.taskConfig = {
+				message: 'Hello, world!'
+			};
+			return cliConfig(args, options)
+				.then(function(returnValue) {
+					var cli = unmockCli();
+
+					expect(returnValue).to.eql({
+						message: 'Hello, world!'
+					});
+					expect(cli.stdout).to.equal(JSON.stringify({
+						message: 'Hello, world!'
+					}, null, 2) + '\n');
+					expect(MockApi.instance.getTaskConfig).to.have.been.calledWith({
+						task: 'task',
+						target: null,
+						package: null,
+						environment: null,
+						expand: false
+					});
+				});
+		});
+
+		it('should get local task configuration, custom target', function() {
+			unmockCli = mockCli();
+
+			var args = ['get'];
+			var options = {
+				task: 'task',
+				target: 'custom'
+			};
+			MockApi.stubs.taskConfig = {
+				message: 'Hello, world!'
+			};
+			return cliConfig(args, options)
+				.then(function(returnValue) {
+					var cli = unmockCli();
+
+					expect(returnValue).to.eql({
+						message: 'Hello, world!'
+					});
+					expect(cli.stdout).to.equal(JSON.stringify({
+						message: 'Hello, world!'
+					}, null, 2) + '\n');
+					expect(MockApi.instance.getTaskConfig).to.have.been.calledWith({
+						task: 'task',
+						target: 'custom',
+						package: null,
+						environment: null,
+						expand: false
+					});
+				});
+		});
+
+		it('should get external task configuration, default target', function() {
+			unmockCli = mockCli();
+
+			var args = ['get'];
+			var options = {
+				task: 'task',
+				package: 'package'
+			};
+			MockApi.stubs.taskConfig = {
+				message: 'Hello, world!'
+			};
+			return cliConfig(args, options)
+				.then(function(returnValue) {
+					var cli = unmockCli();
+
+					expect(returnValue).to.eql({
+						message: 'Hello, world!'
+					});
+					expect(cli.stdout).to.equal(JSON.stringify({
+						message: 'Hello, world!'
+					}, null, 2) + '\n');
+					expect(MockApi.instance.getTaskConfig).to.have.been.calledWith({
+						task: 'task',
+						target: null,
+						package: 'package',
+						environment: null,
+						expand: false
+					});
+				});
+		});
+
+		it('should get external task configuration, custom target', function() {
+			unmockCli = mockCli();
+
+			var args = ['get'];
+			var options = {
+				task: 'task',
+				target: 'custom',
+				package: 'package'
+			};
+			MockApi.stubs.taskConfig = {
+				message: 'Hello, world!'
+			};
+			return cliConfig(args, options)
+				.then(function(returnValue) {
+					var cli = unmockCli();
+
+					expect(returnValue).to.eql({
+						message: 'Hello, world!'
+					});
+					expect(cli.stdout).to.equal(JSON.stringify({
+						message: 'Hello, world!'
+					}, null, 2) + '\n');
+					expect(MockApi.instance.getTaskConfig).to.have.been.calledWith({
+						task: 'task',
+						target: 'custom',
+						package: 'package',
+						environment: null,
+						expand: false
+					});
+				});
+		});
+
+		it('should pass the project path to the API', function() {
+			unmockCli = mockCli();
+
+			var args = ['get'];
+			var options = {
+				config: {},
+				path: '/project'
+			};
+			return cliConfig(args, options)
+				.then(function(returnValue) {
+					unmockCli();
+					expect(MockApi.instance.path).to.equal('/project');
+				});
+		});
+
+		it('should default to process.cwd() if no project path is specified', function() {
+			unmockCli = mockCli();
+
+			var args = ['get'];
+			var options = {
+				config: {}
+			};
+			return cliConfig(args, options)
+				.then(function(returnValue) {
+					unmockCli();
+					expect(MockApi.instance.path).to.equal(process.cwd());
+				});
+		});
 	});
 
-	it('should update task configuration if a local task is specified', function() {
-		var args = [];
-		var options = {
-			task: 'hello',
-			target: 'custom',
-			config: {
-				hello: 'world'
-			},
-			environment: 'goodbye'
-		};
-		var expected = {
-			task: 'hello',
-			target: 'custom',
-			package: null,
-			updates: {
-				hello: 'world'
-			},
-			environment: 'goodbye'
-		};
-		return cliConfig(args, options)
-			.then(function(returnValue) {
-				expect(MockApi.instance.updateTaskConfig).to.have.been.calledWith(expected);
-			});
-	});
+	describe('config set', function() {
 
-	it('should update task configuration if an external task is specified', function() {
-		var args = [];
-		var options = {
-			task: 'hello',
-			target: 'custom',
-			package: 'my-package',
-			config: {
-				hello: 'world'
-			},
-			environment: 'goodbye'
-		};
-		var expected = {
-			task: 'hello',
-			target: 'custom',
-			package: 'my-package',
-			updates: {
-				hello: 'world'
-			},
-			environment: 'goodbye'
-		};
-		return cliConfig(args, options)
-			.then(function(returnValue) {
-				expect(MockApi.instance.updateTaskConfig).to.have.been.calledWith(expected);
-			});
-	});
+		it('should throw an error if no configuration updates are specified', function() {
+			var args = ['set'];
+			var options = {
+				env: 'goodbye'
+			};
+			var promise = cliConfig(args, options);
+			return expect(promise).to.be.rejectedWith(InvalidArgumentsError);
+		});
 
-	it('should pass the project path to the API', function() {
-		var args = [];
-		var options = {
-			config: {},
-			path: '/project'
-		};
-		return cliConfig(args, options)
-			.then(function(returnValue) {
-				expect(MockApi.instance.path).to.equal('/project');
-			});
-	});
+		it('should update default environment configuration when no package is specified', function() {
+			var args = ['set'];
+			var options = {
+				config: {
+					hello: 'world'
+				},
+				env: 'goodbye'
+			};
+			return cliConfig(args, options)
+				.then(function(returnValue) {
+					expect(MockApi.instance.updateEnvironmentConfig).to.have.been.calledWith({
+						updates: {
+							hello: 'world'
+						},
+						environment: 'goodbye'
+					});
+				});
+		});
 
-	it('should default to process.cwd() if no project path is specified', function() {
-		var args = [];
-		var options = {
-			config: {}
-		};
-		return cliConfig(args, options)
-			.then(function(returnValue) {
-				expect(MockApi.instance.path).to.equal(process.cwd());
-			});
+		it('should update package configuration if an external package is specified', function() {
+			var args = ['set'];
+			var options = {
+				package: 'my-package',
+				config: {
+					hello: 'world'
+				},
+				env: 'goodbye'
+			};
+			return cliConfig(args, options)
+				.then(function(returnValue) {
+					expect(MockApi.instance.updatePackageConfig).to.have.been.calledWith({
+						package: 'my-package',
+						updates: {
+							hello: 'world'
+						},
+						environment: 'goodbye'
+					});
+				});
+		});
+
+		it('should update task configuration if a local task is specified', function() {
+			var args = ['set'];
+			var options = {
+				task: 'hello',
+				target: 'custom',
+				config: {
+					hello: 'world'
+				},
+				env: 'goodbye'
+			};
+			return cliConfig(args, options)
+				.then(function(returnValue) {
+					expect(MockApi.instance.updateTaskConfig).to.have.been.calledWith({
+						task: 'hello',
+						target: 'custom',
+						package: null,
+						updates: {
+							hello: 'world'
+						},
+						environment: 'goodbye'
+					});
+				});
+		});
+
+		it('should update task configuration if an external task is specified', function() {
+			var args = ['set'];
+			var options = {
+				task: 'hello',
+				target: 'custom',
+				package: 'my-package',
+				config: {
+					hello: 'world'
+				},
+				env: 'goodbye'
+			};
+			return cliConfig(args, options)
+				.then(function(returnValue) {
+					expect(MockApi.instance.updateTaskConfig).to.have.been.calledWith({
+						task: 'hello',
+						target: 'custom',
+						package: 'my-package',
+						updates: {
+							hello: 'world'
+						},
+						environment: 'goodbye'
+					});
+				});
+		});
+
+		it('should pass the project path to the API', function() {
+			var args = ['set'];
+			var options = {
+				config: {},
+				path: '/project'
+			};
+			return cliConfig(args, options)
+				.then(function(returnValue) {
+					expect(MockApi.instance.path).to.equal('/project');
+				});
+		});
+
+		it('should default to process.cwd() if no project path is specified', function() {
+			var args = ['set'];
+			var options = {
+				config: {}
+			};
+			return cliConfig(args, options)
+				.then(function(returnValue) {
+					expect(MockApi.instance.path).to.equal(process.cwd());
+				});
+		});
 	});
 });
